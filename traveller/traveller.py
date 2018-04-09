ed45c435d9d6e13384dca3624bf60e005f192e95
Przemysław Burzykowski <202126@edu.p.lodz.pl>
2018-04-09 23:45:06 +0200

    Revert "Rewrited traveller"
    This reverts commit dfe89fb6d236084354eb3d2d82b9aa0e57b290b8.

--- traveller/traveller.py
+++ traveller/traveller.py
@@ -1,8 +1,7 @@
 import random
 import math
-import itertools
-import numpy as np
 import copy
+
 
 class Point:
     maxRange = 100
@@ -22,12 +21,9 @@ class Point:
         return 'Point: [{self.x}, {self.y}]'.format(self=self)
 
 
-class Node:
-    id = 0
-
-    def __init__(self, point=None):
-        self.id = Node.id
-        Node.id = Node.id + 1
+class City:
+    def __init__(self, id, point=None):
+        self.id = id
         if point is None:
             self.point = Point()
         else:
@@ -37,11 +33,26 @@ class Node:
         return 'City: [id: {self.id}, {self.point}]'.format(self=self)
 
 
+class Cities:
+    def __init__(self):
+        self.cityList: City = list()
+        self.id: int = 0
+
+    def generate_next_city(self, how: int = 1):
+        for i in range(0, how):
+            self.cityList.append(City(self.id))
+            self.id += 1
+
+
+class Node:
+    pass
+
+
 class Relation:
     def __init__(self, start: Node, destination: Node):
-        self.start: Node = start
-        self.destination: Node = destination
-        self.distance = self.calculate_distance(start.point, destination.point)
+        self.start:Node = start
+        self.destination:Node = destination
+        self.distance = self.calculate_distance(start.data.point, destination.data.point)
 
     @staticmethod
     def calculate_distance(p1: Point, p2: Point):
@@ -49,125 +60,212 @@ class Relation:
         len_y = p1.y - p1.y
         return math.sqrt(math.pow(len_x, 2) + math.pow(len_y, 2))
 
+    def is_visited(self) -> bool:
+        return self.start.is_visited == True and self.destination.is_visited == True
+
+    def mark_as_visited(self, is_visited:bool):
+        self.start.is_visited = is_visited
+        self.destination.is_visited = is_visited
+
     def __str__(self):
         return 'Start: [{self.start}] Destination [{self.destination}] distance: {self.distance}'.format(self=self)
 
 
-class Path:
+class Node:
+    def __init__(self, data):
+        self.data:City = data
+        self.connections: list = list()
+        self.is_visited: bool = False
 
-    def __init__(self):
-        self.relations = []
-        self.total_distance = 0
-
-    def add(self, relation: Relation):
-        self.relations.append(relation)
-        self.total_distance += relation.distance
-
-
-class Area:
-    def __init__(self, size: int):
-        self.nodes = []
-        self.generate_nodes(size)
-        self.relations = []
-
-    def generate_nodes(self, size: int):
-        for i in range(0, size):
-            self.nodes.append(Node())
-
-    def generate_one_to_all_relations(self):
-        size = len(self.nodes)
-        for const_id in range(0, size - 1):
-            for next_id in range(const_id + 1, size):
-                start = self.nodes[const_id]
-                destination = self.nodes[next_id]
-                relation = Relation(start, destination)
-                self.relations.append(relation)
-
-    def nodes_str(self):
-        s = "\n"
-        for node in self.nodes:
-            s += str(node) + "\n"
-        return s
-
-    def relations_str(self):
-        s = "\n"
-        for relation in self.relations:
-            s += str(relation) + "\n"
-        return s
+    def add_relation(self, *children: Relation):
+        for c in children:
+            self.connections.append(c)
 
     def __str__(self):
-        return self.nodes_str() + self.relations_str()
+        return 'Node: [{self.data}, is_visited: {self.is_visited}]'.format(self=self)
+
+
+class Nodes:
+    nodeList:Node = list()
+
+    def __init__(self, cities = None):
+        if(cities != None):
+            for city in cities.cityList:
+                self.nodeList.append(Node(city))
+
+    def clear_is_visited(self):
+        for node in self.nodeList:
+            node.is_visited = False
+
+
+class Relations:
+
+    def __init__(self, nodes: Nodes = None):
+        if (nodes != None):
+            self.relationList = list()
+            self.generate(copy.copy(nodes.nodeList))
+        else:
+            self.relationList = list()
+
+    def generate(self, nodes):
+        while len(nodes) > 1:
+            start_node = nodes[0]
+            del (nodes[0])
+            for i in range(0, len(nodes)):
+                relation = Relation(start_node, nodes[i])
+                self.relationList.append(relation)
+
+    def get_min_distance(self, id:int = None) -> Relation:
+        if id is None:
+            searchingList = self.relationList
+        else:
+            searchingList = self.get_relations_with_city(id)
+
+        if len(searchingList) == 0:
+            return None
+
+        min_relation: Relation = searchingList[0]
+
+        for relation in searchingList:
+            if relation.distance < min_relation.distance:
+                min_relation = relation
+        return min_relation
+
+    def get_available_relation_from(self, id):
+        available_relations = Relations()
+        for relation in self.relationList:
+            if (relation.destination.data.id == id or relation.start.data.id == id) and not relation.is_visited():
+                available_relations.relationList.append(relation)
+        return available_relations
+
+    # mark as dangerous
+    def get_relations_without_city(self, id, is_visited = False):
+        relations = list();
+        for relation in self.relationList:
+            if relation.destination.data.id != id and relation.start.data.id != id:
+                relations.append(relation)
+        return relations
+
+    # mark as dangerous
+    def get_relations_with_city(self, id):
+        relations = list()
+        for relation in self.relationList:
+            if relation.destination.data.id == id or relation.start.data.id == id:
+                relations.append(relation)
+        return relations
+
+    def mark_all_visited(self, is_visited:bool):
+        for relation in relations.relationList:
+            relation.mark_as_visited(False)
+
+
+class Path:
+    relations = list()
+
+    def get_distance(self):
+        distance = 0
+        print(len(relations.relationList))
+        for relation in self.relations:
+            distance += relation.distance
+        return distance
+
+
+class SmallestCost:
+    path:Path = Path()
+
+    def __init__(self, relations:Relations):
+        self.relations = copy.copy(relations)
+        self.caluclate()
+
+    def caluclate(self):
+        relations.mark_all_visited(False)
+        self.path.relations.clear()
+
+        shortest_relation = relations.get_min_distance()
+        self.path.relations.append(shortest_relation)
+        shortest_relation.mark_as_visited(True)
+        next_id = shortest_relation.destination.data.id
+
+        while True:
+            relations_from_node = relations.get_available_relation_from(next_id)
+
+            if relations_from_node is None or len(relations_from_node.relationList) is 0:
+                break
+
+            shortest_relation = relations_from_node.get_min_distance()
+            self.path.relations.append(shortest_relation)
+
+            if shortest_relation.destination.is_visited is False:
+                next_id = shortest_relation.destination.data.id
+            elif shortest_relation.start.is_visited is False:
+                next_id = shortest_relation.start.data.id
+
+            shortest_relation.mark_as_visited(True)
+
+    def __str__(self):
+        s = ""
+        for relation in self.path.relations:
+            s += str(relation) + "\n"
+        s += "Total distance: " + str(self.path.get_distance()) + "\n"
+        return s
 
 
 class BruteForce:
-    def __init__(self, area: Area):
-        self.area = area
-        self.paths = []
-        self.min_path: Path = None
-        self.max_path: Path = None
 
-    def run(self):
-        area_size = len(self.area.nodes)
-        id_list = np.arange(area_size)
-        permutations = itertools.permutations(id_list)
-        for permutation in permutations:
-            path = Path()
-            for i in range(0, len(permutation) - 1):
-                start = self.area.nodes[permutation[i]]
-                destination = self.area.nodes[permutation[i+1]]
-                relation = Relation(start, destination)
-                path.add(relation)
-            self.paths.append(path)
-        self.generate_info()
+    def __init__(self, relations):
+        self.relations = copy.copy(relations)
+        self.nodes_id = list()
+        self.generate_id()
+        self.permutation_pare_index = 0
 
-    def generate_info(self):
-        self.min_path = min(self.paths, key=lambda path: path.total_distance)
-        self.max_path = max(self.paths, key=lambda path: path.total_distance)
+    def generate_id(self):
+        for relation in self.relations.relationList:
+            is_already_id_in_list = [False, False]
+            for id in self.nodes_id:
+                if relation.start.data.id is id:
+                    is_already_id_in_list[0] = True
+                if relation.destination.data.id is id:
+                    is_already_id_in_list[1] = True
+                if is_already_id_in_list[0] is True and is_already_id_in_list[1] is True:
+                    break
 
-    def __str__(self):
-        return "Brtutforce method: \n Min: " + str(self.min_path.total_distance) \
-               + "\n Max: " + str(self.max_path.total_distance)
+            if is_already_id_in_list[0] is False:
+                self.nodes_id.append(relation.start.data.id)
+            if is_already_id_in_list[1] is False:
+                self.nodes_id.append(relation.destination.data.id)
 
-class SmallestCost:
+        self.nodes_id = sorted(self.nodes_id)
 
-    def __init__(self, area: Area):
-        self.area = area
-        self.path = Path()
+    def next_permutation(self):
+        tmp = self.nodes_id[self.permutation_pare_index]
+        self.nodes_id[self.permutation_pare_index] = self.nodes_id[self.permutation_pare_index+1]
+        self.nodes_id[self.permutation_pare_index+1] = tmp
+        self.permutation_pare_index += 1
+        if self.permutation_pare_index < len(self.nodes_id):
+            self.permutation_pare_index = 0
 
-    def run(self):
-        id_list = list(np.arange(len(self.area.nodes)))
+cities = Cities()
+cities.generate_next_city(3)
+nodes = Nodes(cities)
+relations = Relations(nodes)
 
-        for i in range(0, len(id_list) - 1):
-            relations = []
-            for j in range(1, len(id_list)):
-                relation = Relation(self.area.nodes[id_list[0]], self.area.nodes[id_list[j]])
-                relations.append(relation)
-            min_relation = min(relations, key=lambda relation: relation.distance)
-            self.path.add(min_relation)
+bf = BruteForce(relations)
+bf.permutate()
 
-            min_id = None
-            if min_relation.destination.id != id_list[0]:
-                min_id = min_relation.destination.id
-            elif min_relation.start.id != id_list[0]:
-                min_id = min_relation.start.id
+'''
+for x in bf.nodes_id:
+    print(x)
+'''
 
-            id_list.pop(0)
-            id_list.remove(min_id)
-            id_list.insert(0, min_id)
-
-    def __str__(self):
-        return "Smallest cost method: \n min distance: " + str(sc.path.total_distance)
+'''
+for x in relations.relationList:
+    print(x)
+'''
 
 
+'''
+smallestCost = SmallestCost(relations)
+smallestCost.caluclate()
 
-a = Area(5)
-bf = BruteForce(a)
-bf.run()
-print(bf)
-
-print()
-sc = SmallestCost(a)
-sc.run()
-print(sc)
-
-
+print(smallestCost)
+'''
